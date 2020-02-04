053481dadea11ba1c0df4a5547aaccf77becb6f2
Andrew Reid <areid.sj@gmail.com>
2020-02-04 11:45:37 -0700

    Write and pass it has a name test

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -9,4 +9,9 @@ class ShipTest < Minitest::Test
     assert_instance_of Ship, cruiser
   end
 
+  def test_it_has_a_name
+    cruiser = Ship.new("Cruiser", 3)
+    assert_equal "Cruiser", cruiser.name
+  end
+
 end
