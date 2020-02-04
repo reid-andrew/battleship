0b0fee52ffe1930dba54a56887820e5111beb03f
Andrew Reid <areid.sj@gmail.com>
2020-02-04 16:15:52 -0700

    Refactor and rename placement tests

--- test/board_test.rb
+++ test/board_test.rb
@@ -33,20 +33,33 @@ class BoardTest < Minitest::Test
     submarine = Ship.new("Submarine", 2)
     assert_equal false, board.valid_placement?(cruiser, ["A1", "A2"])
     assert_equal false, board.valid_placement?(submarine, ["A2", "A3", "A4"])
-    assert_equal true, board.valid_placement?(cruiser, ["A1", "A2", "A3"])
-    assert_equal true, board.valid_placement?(submarine, ["A2", "A3"])
   end
 
   def test_it_validates_consecutive_coordinates
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
-    require "pry"; binding.pry
     assert_equal false, board.valid_placement?(cruiser, ["A1", "A2", "A4"])
     assert_equal false, board.valid_placement?(submarine, ["A1", "C1"])
     assert_equal false, board.valid_placement?(cruiser, ["A3", "A2", "A1"])
     assert_equal false, board.valid_placement?(submarine, ["C1", "B1"])
+  end
+
+  def test_it_validates_diagonal_coordinates
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+    assert_equal false, board.valid_placement?(cruiser, ["A1", "B2", "C3"])
+    assert_equal false, board.valid_placement?(submarine, ["C2", "D3"])
+  end
+
+  def test_it_validates_correct_placement
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
     assert_equal true, board.valid_placement?(cruiser, ["A1", "A2", "A3"])
-    assert_equal true, board.valid_placement?(submarine, ["C1", "D1"])    
+    assert_equal true, board.valid_placement?(submarine, ["A1", "A2"])
+    assert_equal true, board.valid_placement?(cruiser, ["B1", "C1", "D1"])
+    assert_equal true, board.valid_placement?(submarine, ["C1", "D1"])
   end
 end
