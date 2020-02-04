b7b0405e1195495435a76b76f9577c0768f5b58b
Andrew Reid <areid.sj@gmail.com>
2020-02-04 15:10:21 -0700

    Write and pass board exists and cells created tests

---
+++ lib/board.rb
@@ -0,0 +1,13 @@
+class Board
+  attr_reader :cells
+
+  def initialize
+    @cells = {}
+      ("A".."D").each do |letter|
+        (1..4).each do |number|
+          @cells["#{letter}#{number}"] = Cell.new("#{letter}#{number}")
+        end
+      end
+  end
+
+end
---
+++ test/board_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/cell'
+require './lib/board'
+
+class BoardTest < Minitest::Test
+
+  def test_it_exists
+    board = Board.new
+    assert_instance_of Board, board
+  end
+
+  def test_it_has_cells
+    board = Board.new
+    assert_instance_of Hash, board.cells
+    assert_equal 16, board.cells.size
+    assert_equal "A2", board.cells["A2"].coordinate
+    assert_equal "C4", board.cells["C4"].coordinate
+  end
+end
