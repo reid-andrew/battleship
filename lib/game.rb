b98e8e3a7516d45ce01823a7301c0f631a3a829d
Andrew Reid <areid.sj@gmail.com>
2020-02-06 16:04:17 -0700

    Add turns method

--- lib/game.rb
+++ lib/game.rb
@@ -46,7 +46,27 @@ class Game
       print "That's an invalid entry" + "\n"
       @player_board.place(@player_sub, player_ship_input("Submarine"))
     end
-    print @player_board.render(true)
+    turns
+  end
+
+  def turns
+    print "Computer Board"
+    print @computer_board.render + "\n"
+    print "Player Board"
+    print @player_board.render(true) + "\n"
+
+    print "On which coordinate would you like to fire?" + "\n"
+    print "> "
+    input_coordinate = player_coordinate_input(gets.chomp)
+
+    @computer_board.cells[input_coordinate].fire_upon
+    if winner_winner_chicken_dinner != "Game continues"
+      print winner_winner_chicken_dinner + "\n"
+      return
+    end
+
+    @player_board.cells[input_coordinate].fire_upon
+    winner_winner_chicken_dinner == "Game continues" ? turns : winner_winner_chicken_dinner + "\n"
   end
 
   def player_ship_input(ship_type)
@@ -65,7 +85,9 @@ class Game
   end
 
   def player_coordinate_input(coordinate)
-    if !@player_board.valid_coordinate?(coordinate)
+    if coordinate == "please"
+      return
+    elsif !@player_board.valid_coordinate?(coordinate)
       print "That's not a valid coordinate. Please try again."
       print "> "
       player_coordinate_input(gets.chomp)
@@ -74,6 +96,14 @@ class Game
     end
   end
 
-
-
+  def winner_winner_chicken_dinner
+    if @player_cruiser.sunk && @player_sub.sunk
+      print "I win! You sunk!"
+    elsif @computer_cruiser.sunk && @computer_sub.sunk
+      print "You win! I sunk!"
+    else
+      "Game continues"
+    end
+  end
+  
 end
