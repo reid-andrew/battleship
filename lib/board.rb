32a5b8a95906a1d2b7a736f00714c741c09064b1
Andrew Reid <areid.sj@gmail.com>
2020-02-07 12:17:11 -0700

    Refactor player ship placement

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,5 @@
+require './lib/cell'
+
 class Board
   attr_reader :cells
 
@@ -37,8 +39,6 @@ class Board
   def place(ship, coordinates)
     if valid_placement?(ship, coordinates)
       coordinates.each { |coordinate| @cells[coordinate].place_ship(ship)}
-    else #Andy added this else/false in - block used to end here to make the runner work.
-      false
     end
   end
 
--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,6 @@
+require './lib/board'
+require './lib/ship'
+
 class Game
 
   def greeting(clear_screen = true)
@@ -19,7 +22,7 @@ class Game
     else
       print "\e[2J\e[f"
       print "\n" + "Invalid response. Enter 'p' to play. Enter 'q' to quit." + "\n" + "> "
-      start(gets.chomp)
+      start(gets.chomp.downcase)
     end
   end
 
@@ -38,35 +41,19 @@ class Game
     print "\n" + "My ships are laid out on the grid. Now it's time to place yours." + "\n" + "\n"
     print "The Cruiser is 3 units long and the Submarine is 2 units long." + "\n" + "\n"
     print @player_board.render + "\n"
-    while @player_board.place(@player_cruiser, player_ship_input("Cruiser")) == false
-      print "That's an invalid entry" + "\n"
-      @player_board.place(@player_cruiser, player_ship_input("Cruiser"))
-    end
-    while @player_board.place(@player_sub, player_ship_input("Submarine")) == false
-      print "That's an invalid entry" + "\n"
-      @player_board.place(@player_sub, player_ship_input("Submarine"))
-    end
+    player_ship_placement(@player_board, @player_cruiser, "Cruiser")
+    player_ship_placement(@player_board, @player_sub, "Submarine")
     turns
   end
 
-  def turns(prior_result = nil)
-    print "\e[2J\e[f"
-    if prior_result
-      print prior_result
+  def player_ship_placement(board, ship, ship_type)
+    board.place(ship, player_ship_input(ship_type))
+    ship_length = ship.length
+    cells_with_ship = @player_board.cells.select {|coord, object| object.ship == ship}
+    if ship_length != cells_with_ship.length
+      print "That's an invalid entry" + "\n"
+      player_ship_placement(board, ship, ship_type)
     end
-    print "Computer Board"
-    print @computer_board.render + "\n"
-    print "Player Board"
-    print @player_board.render(true) + "\n"
-
-    print "On which coordinate would you like to fire?" + "\n"
-    print "> "
-    input_coordinate = player_coordinate_input(gets.chomp.capitalize)
-    @computer_board.cells[input_coordinate].fire_upon
-    @player_board.cells[input_coordinate].fire_upon
-    turn_result = "\n" + "Your shot on #{input_coordinate} was a #{@computer_board.cells[input_coordinate].render_readable}" +
-    "\n" + "My shot on #{input_coordinate} was a #{@player_board.cells[input_coordinate].render_readable}" + "\n" + "\n"
-    winner == "Game continues" ? turns(turn_result) : winner
   end
 
   def player_ship_input(ship_type)
@@ -85,13 +72,33 @@ class Game
 
   def player_coordinate_input(coordinate)
     if coordinate == "!!!"
-      abort("We're going down, Captain")
+      abort("\n" + "We're going down, Captain!!!" + "\n" + "\n"+ "\n")
     elsif !@player_board.valid_coordinate?(coordinate)
       print "That's not a valid coordinate. Please try again." + "> "
-      player_coordinate_input(gets.chomp)
+      player_coordinate_input(gets.chomp.capitalize)
     else
       coordinate
     end
+  end
+
+  def turns(prior_result = nil)
+    print "\e[2J\e[f"
+    print "Computer Board" + "\n"
+    print @computer_board.render + "\n"
+    print "Player Board" + "\n"
+    print @player_board.render(true) + "\n"
+    if prior_result
+      print prior_result
+    end
+
+    print "On which coordinate would you like to fire?" + "\n"
+    print "> "
+    input_coordinate = player_coordinate_input(gets.chomp.capitalize)
+    @computer_board.cells[input_coordinate].fire_upon
+    @player_board.cells[input_coordinate].fire_upon
+    turn_result = "\n" + "Your shot on #{input_coordinate} was a #{@computer_board.cells[input_coordinate].render_readable}" +
+    "\n" + "My shot on #{input_coordinate} was a #{@player_board.cells[input_coordinate].render_readable}" + "\n" + "\n"
+    winner == :game_continues ? turns(turn_result) : winner
   end
 
   def winner
@@ -99,14 +106,14 @@ class Game
       print "\e[2J\e[f"
       print "I win!" + "\n"
       greeting(false)
-      start(gets.chomp)
+      start(gets.chomp.downcase)
     elsif @computer_cruiser.sunk && @computer_sub.sunk
       print "\e[2J\e[f"
       puts "You win!" + "\n"
       greeting(false)
-      start(gets.chomp)
+      start(gets.chomp.downcase)
     else
-      "Game continues"
+      :game_continues
     end
   end
 end
